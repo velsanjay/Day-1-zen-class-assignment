69a798c9872a8268c075ec5288e589c5a172ef8a
velsanjay <121847438+velsanjay@users.noreply.github.com>
2023-01-20 18:40:42 +0530

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,15 @@
+var request=new XMLHttpRequest();
+
+request.open("GET","https://restcountries.com/v3.1/all");
+
+request.send();
+
+request.onload = function () {
+    var result=JSON.parse(request.response);
+    for (let i=0; i<result.length ; i++){
+        console.log(result[i].flag);
+        console.log(result[i].name.common,result[i].region,result[i].subregion,result[i].population);
+
+    }
+
+}
